19dc4f53e18736361d69c6f245b01ff6ccbc5627
joao4xz <joao.4xz@gmail.com>
2023-05-14 17:42:28 -0300

    Make the code cleaner

--- script.js
+++ script.js
@@ -12,23 +12,18 @@ numButtons.forEach( (button) => {
     if(numberFlag === 2){
       firstNumber = '';
       secondNumber = '';
-      while (display.firstChild) {
-        display.firstChild.remove();
-      }
+      cleanScreen();
       firstNumber = firstNumber.concat(button.textContent);
       display.appendChild(document.createTextNode(button.textContent));
-      console.log(firstNumber);
       numberFlag = 1;
     }
     else if(numberFlag === 1){
       firstNumber = firstNumber.concat(button.textContent);
       display.appendChild(document.createTextNode(button.textContent));
-      console.log(firstNumber);
     }
     else if (numberFlag === 0){
       secondNumber = secondNumber.concat(button.textContent);
       display.appendChild(document.createTextNode(button.textContent));
-      console.log(secondNumber);
     }
   });
 });
@@ -39,33 +34,23 @@ operatorButtons.forEach( (button) => {
       operator = button.textContent;
       display.appendChild(document.createTextNode(button.textContent));
       numberFlag = 0;
-      console.log(operator);
     }
     else if(numberFlag === 0){
-      console.log(`${firstNumber}${operator}${secondNumber}`);
       operate(operator, parseInt(firstNumber), parseInt(secondNumber));
       operator = button.textContent;
       display.appendChild(document.createTextNode(button.textContent));
       numberFlag = 0;
-      console.log(operator);
     }
   })
 });
 
 equal.addEventListener('click', () => {
   if(firstNumber && secondNumber){
-    console.log(`${firstNumber}${operator}${secondNumber}`);
     operate(operator, parseInt(firstNumber), parseInt(secondNumber));
   }
   else{
-    while (display.firstChild) {
-      display.firstChild.remove();
-    }
-    display.appendChild(document.createTextNode("ERRO"));
-    firstNumber = "";
-    secondNumber = "";
-    operator = "";
-    result = 0;
+    cleanEverything();
+    error();
     numberFlag = 2;
   }
 });
@@ -82,37 +67,19 @@ function operate(operator, num1,  num2){
               break;  
   }
   if(operator === '/' && num2 === 0){
-    while (display.firstChild) {
-      display.firstChild.remove();
-    }
-    display.appendChild(document.createTextNode("ERRO"));
-    firstNumber = "";
-    secondNumber = "";
-    operator = "";
-    result = 0;
+    cleanEverything();
+    error();
     numberFlag = 2;
     return;
   }
-  while (display.firstChild) {
-    display.firstChild.remove();
-  }
+  cleanScreen();
   display.appendChild(document.createTextNode(result));
   firstNumber = String(result);
   secondNumber = '';
   numberFlag = 2;
-  console.log(result);
 }
 
-clear.addEventListener('click', () => {
-  firstNumber = "";
-  secondNumber = "";
-  operator = "";
-  result = 0;
-  numberFlag = 1;
-  while (display.firstChild) {
-    display.firstChild.remove();
-  }
-});
+clear.addEventListener('click', cleanEverything);
 
 
 function add(num1, num2){
@@ -126,4 +93,23 @@ function multiply(num1, num2){
 }
 function divide(num1, num2){
   return Math.floor(num1 / num2);
+}
+
+function cleanScreen(){
+  while (display.firstChild) {
+    display.firstChild.remove();
+  }
+}
+
+function cleanEverything(){
+  firstNumber = "";
+  secondNumber = "";
+  operator = "";
+  result = 0;
+  numberFlag = 1;
+  cleanScreen();
+}
+
+function error(){
+  display.appendChild(document.createTextNode("ERRO"));
 }
